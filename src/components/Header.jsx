5d4f7bd4020f399f0630e6ceae263637a6407a62
amandeep <samandeepkaue34@gmail.com>
2024-05-15 18:23:32 -0400

    updated description text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,9 @@ export default function Header() {
     return (
       <header>
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
+        <h1>AMANDEEP KAUR SANDHU</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are going to build!
+          Full Stack Web Developer
         </p>
       </header>
     );

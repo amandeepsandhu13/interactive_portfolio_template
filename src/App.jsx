73ad3902bc1f6b1067621b76bfef2ae64a3b74ef
amandeep <samandeepkaue34@gmail.com>
2024-05-15 18:49:46 -0400

    updated core_concept parameters

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ import { CORE_PROJECTS } from "./data.js";
 //   return Math.floor(Math.random() * (max + 1));
 // }
 
-function CoreConcept(props){
+function CoreConcept({image,title,description}){
   return(
-    <li><img src={props.image} alt="props.title" />
-        <h3>{props.title}</h3>
-        <p>{props.description}</p>
+    <li><img src={image} alt={title} />
+        <h3>{title}</h3>
+        <p>{description}</p>
     </li>
   );
 }
@@ -25,18 +25,9 @@ function App() {
         <h2>Projects</h2>
           <ul>
             <CoreConcept {...CORE_PROJECTS[0]} />
-            <CoreConcept title = {CORE_PROJECTS[1].title}
-                           image = {CORE_PROJECTS[1].image}
-                           description = {CORE_PROJECTS[1].description}
-            />
-             <CoreConcept title = {CORE_PROJECTS[2].title}
-                           image = {CORE_PROJECTS[2].image}
-                           description = {CORE_PROJECTS[2].description}
-            />
-            <CoreConcept title = {CORE_PROJECTS[3].title}
-                           image = {CORE_PROJECTS[3].image}
-                           description = {CORE_PROJECTS[3].description}
-            />
+            <CoreConcept {...CORE_PROJECTS[1]} />
+            <CoreConcept {...CORE_PROJECTS[2]} />
+            <CoreConcept {...CORE_PROJECTS[3]} />
           
           </ul>
         </section>

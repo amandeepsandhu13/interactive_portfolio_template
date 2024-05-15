7e2be930a1c4de8ebf40fa8678ae9aaa24dc8fcd
amandeep <samandeepkaue34@gmail.com>
2024-05-15 18:50:59 -0400

    added profile image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import profileImg from '../assets/profile.JPG';
 
 export default function Header() {
     
     return (
       <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={profileImg} alt="Stylized atom" />
         <h1>AMANDEEP KAUR SANDHU</h1>
         <p>
           Full Stack Web Developer
